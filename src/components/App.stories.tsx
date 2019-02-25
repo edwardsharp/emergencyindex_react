21e28834f6e98225d8537cded1dc481d48ec4e5e
edward sharp <edward@edwardsharp.net>
2019-02-24 23:50:20 -0500

    tidy docz :swimmer:

--- src/components/App.stories.tsx
+++ src/components/App.stories.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
 import { setAddon, storiesOf } from '@storybook/react';
 import { linkTo } from '@storybook/addon-links';
 import JSXAddon from 'storybook-addon-jsx';
-
 import { Welcome } from '@storybook/react/demo';
+import Button from './Button'
 
 setAddon(JSXAddon);
 
 storiesOf('Welcome', module)
-.add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+.add('to Storybook', () => <>
+  <h1>Emergency INDEX</h1>
+  <h3>storybook</h3>
+
+  <p>This is a UI component dev environment for the emergencyindex react app.</p>
+  <p>A story is a single state of one or more UI components. Basically a story is like a visual test case.</p>
+  
+</>);
