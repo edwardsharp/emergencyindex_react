cee016ac2988fe030dfc8866e30c7b09c723c300
edward sharp <edward@edwardsharp.net>
2019-02-27 00:07:03 -0500

    more fake

--- src/__test__/projects.ts
+++ src/__test__/projects.ts
@@ -7,11 +7,11 @@ export const projects:iProject[] = [
     image: faker.image.cats(),
     photo_credit: `${faker.name.firstName()} ${faker.name.lastName()}`,
     title: `${faker.hacker.adjective()} ${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-    first_performed: `${faker.date.month()} ${faker.random.number(28)}, 2011`,
+    first_performed: `${faker.date.month()} ${Math.max(1, faker.random.number(28))}, 2011`,
     place: `${[faker.commerce.productMaterial(), faker.hacker.verb().toUpperCase(), faker.hacker.abbreviation()][faker.random.number(2)]} ${['Gallery','Art Institute','Club','Space'][faker.random.number(3)]}, ${faker.address.city()}, ${faker.address.countryCode()}`,
     times_performed: faker.random.number(10).toString(),
     contributor: `${faker.name.firstName()} ${faker.name.lastName()}`,
-    collaborators: [`${faker.name.firstName()} ${faker.name.lastName()}`, `${faker.name.firstName()} ${faker.name.lastName()}`],
+    collaborators: Array.from({length: faker.random.number(6)}, () => `${faker.name.firstName()} ${faker.name.lastName()}`),
     home: `${faker.address.county()}`,
     links: [`${faker.internet.url()}`],
     contact: `${faker.internet.email()}`,
@@ -19,6 +19,6 @@ export const projects:iProject[] = [
     tags: [`${faker.hacker.ingverb()}`, `${faker.hacker.ingverb()}`, `${faker.hacker.ingverb()}`],
     pages: `002-003`,
     needs_review: true,
-    description: `${faker.hacker.phrase()}\n${faker.lorem.paragraphs(Math.min(2, faker.random.number(7)))}`
+    description: `${faker.hacker.phrase()} ${faker.hacker.phrase()}\n${faker.lorem.paragraphs(Math.max(4, faker.random.number(10)))}`
   }
 ]
