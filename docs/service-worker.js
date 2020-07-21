57dac3adcec6d7f7bf0b25b885c725b52eeeca9c
eeddee <edward@edwardsharp.net>
2020-07-20 23:21:01 -0400

    update /docs/

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.93cda23718550b0a308b208e32f06afa.js"
+  "precache-manifest.2e0c802e669613e9f5a4e282531266a0.js"
 );
 
 self.addEventListener('message', (event) => {
