3a8f5c719ed464004e5c5f393dd1fe9a3702ba9a
eeddee <edward@edwardsharp.net>
2020-07-26 00:52:50 -0400

    update packages && tidy'n

--- src/components/project/Project.stories.tsx
+++ src/components/project/Project.stories.tsx
@@ -17,16 +17,3 @@ export const example = () => {
   })
   return <Project project={project} />
 }
-
-// example.story = {
-//   parameters: {
-//     knobs: {
-//       // Doesn't emit events while user is typing.
-//       timestamps: true,
-
-//       // Escapes strings to be safe for inserting as innerHTML. This option is true by default. It's safe to set it to `false` with frameworks like React which do escaping on their side.
-//       // You can still set it to false, but it's strongly discouraged to set to true in cases when you host your storybook on some route of your main site or web app.
-//       escapeHTML: true,
-//     },
-//   },
-// }
