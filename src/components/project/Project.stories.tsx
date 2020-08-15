583d43f40cbee9186353721ffa01c09935cd7295
eeddee <edward@edwardsharp.net>
2020-08-15 19:29:40 -0400

    tidy project stories

--- src/components/project/Project.stories.tsx
+++ src/components/project/Project.stories.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { withKnobs, button } from '@storybook/addon-knobs'
 
 import Project from './Project'
+import Meta from './Meta'
+import Description from './Description'
+import Footer from './Footer'
+
 import { generateFakeProject } from '../../__test__/projects'
 
 export default {
@@ -9,11 +13,34 @@ export default {
   decorators: [withKnobs],
 }
 
-export const example = () => {
+export const project = () => {
   let project = generateFakeProject()
-
   button('randomize project text', () => {
     project = generateFakeProject()
   })
   return <Project project={project} />
+}
+
+export const meta = () => {
+  let project = generateFakeProject()
+  button('randomize project text', () => {
+    project = generateFakeProject()
+  })
+  return <Meta project={project} />
+}
+
+export const description = () => {
+  let project = generateFakeProject()
+  button('randomize project text', () => {
+    project = generateFakeProject()
+  })
+  return <Description project={project} />
+}
+
+export const footer = () => {
+  let project = generateFakeProject()
+  button('randomize project text', () => {
+    project = generateFakeProject()
+  })
+  return <Footer project={project} />
 }
