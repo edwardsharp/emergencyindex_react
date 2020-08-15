7a01a1a8eb389a2fd196fe1e32b25b79340c24a6
eeddee <edward@edwardsharp.net>
2020-08-15 19:57:53 -0400

    more fffake

--- src/__test__/projects.ts
+++ src/__test__/projects.ts
@@ -3,36 +3,46 @@ import iProject from '../interfaces/project'
 
 export const generateFakeProject = (): iProject => ({
   volume: '2011',
-  image: faker.image.cats(),
+  image: `${faker.image.cats()}?cachebustr=${Math.random()}`,
   photo_credit: `${faker.name.firstName()} ${faker.name.lastName()}`,
   title: `${faker.hacker.adjective()} ${faker.hacker.adjective()} ${faker.hacker.noun()}`,
   first_performed: `${faker.date.month()} ${Math.max(
     1,
     faker.random.number(28)
   )}, 2011`,
-  place: `${
-    [
-      faker.commerce.productMaterial(),
-      faker.hacker.verb().toUpperCase(),
-      faker.hacker.abbreviation(),
-    ][faker.random.number(2)]
-  } ${
-    ['Gallery', 'Art Institute', 'Club', 'Space'][faker.random.number(3)]
-  }, ${faker.address.city()}, ${faker.address.countryCode()}`,
+  place: `${faker.random.arrayElement([
+    faker.commerce.productMaterial(),
+    faker.hacker.verb().toUpperCase(),
+    faker.hacker.abbreviation(),
+  ])} ${faker.random.arrayElement([
+    'Gallery',
+    'Art Institute',
+    'Club',
+    'Space',
+    'Spot',
+    'Hub',
+    'Joint',
+  ])}, ${faker.address.city()}, ${faker.address.countryCode()}`,
   times_performed: faker.random.number(10).toString(),
   contributor: `${faker.name.firstName()} ${faker.name.lastName()}`,
   collaborators: Array.from(
     { length: faker.random.number(6) },
     () => `${faker.name.firstName()} ${faker.name.lastName()}`
   ),
-  home: `${faker.address.county()}`,
-  links: [`${faker.internet.url()}`],
-  contact: `${faker.internet.email()}`,
+  home: faker.address.county(),
+  links: [faker.internet.url()],
+  contact: faker.internet.email(),
   footnote: ``,
   tags: [
-    `${faker.hacker.ingverb()}`,
-    `${faker.hacker.ingverb()}`,
-    `${faker.hacker.ingverb()}`,
+    ...Array.from({ length: faker.random.number(5) }, () =>
+      faker.hacker.adjective()
+    ),
+    ...Array.from({ length: faker.random.number(5) }, () =>
+      faker.hacker.verb()
+    ),
+    ...Array.from({ length: faker.random.number(5) }, () =>
+      faker.hacker.noun()
+    ),
   ],
   pages: `002-003`,
   needs_review: true,
