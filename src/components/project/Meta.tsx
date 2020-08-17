a75724a696c715b9dbe689c23f812c6cdb55af7b
eeddee <edward@edwardsharp.net>
2020-08-16 22:31:12 -0400

    fix collaborator hotlink :feelsgood:

--- src/components/project/Meta.tsx
+++ src/components/project/Meta.tsx
@@ -72,7 +72,7 @@ export default function Meta(props: {
               onClick={(event) => {
                 event.preventDefault()
                 event.stopPropagation()
-                setQuery(project.home)
+                setQuery(collaborator)
               }}
             >
               {collaborator}
