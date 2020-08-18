4a87e5ffc43c33af3622f99ca3f2df6de8f84581
eeddee <edward@edwardsharp.net>
2020-08-17 22:31:39 -0400

    storybook build :construction:

---
+++ docs/precache-manifest.ceb81ea40799798b69bf2b7534d2e40f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a2870d11775690ec73751ef108afa8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.76de9f4dcac952868485.bundle.js"
+  },
+  {
+    "url": "runtime~main.76de9f4dcac952868485.bundle.js"
+  },
+  {
+    "revision": "fca306b2991e40f4ccae",
+    "url": "static/css/main.343b6c58.chunk.css"
+  },
+  {
+    "url": "vendors~main.76de9f4dcac952868485.bundle.js"
+  },
+  {
+    "url": "vendors~main.76de9f4dcac952868485.bundle.js.LICENSE.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3204c9072f0473f5fa1d38d963eb2647.js"
+  "precache-manifest.ceb81ea40799798b69bf2b7534d2e40f.js"
 );
 
 self.addEventListener('message', (event) => {
