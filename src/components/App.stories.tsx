5aec4f799814442ef09c3fcae7710df1b67398ec
eeddee <edward@edwardsharp.net>
2020-08-22 15:04:52 -0400

    update app story

--- src/components/App.stories.tsx
+++ src/components/App.stories.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 storiesOf('Introduction', module).add('to Storybook', () => (
-  <>
+  <div style={{ margin: '2em' }}>
     <h1>Emergency INDEX</h1>
     <h3>storybook</h3>
 
+    <p>This is a UI component library for the emergencyindex react app.</p>
     <p>
-      This is a UI component dev environment for the emergencyindex react app.
+      A story is a single example of one or more UI components. Basically a
+      story is like a visual test case.
     </p>
-    <p>
-      A story is a single state of one or more UI components. Basically a story
-      is like a visual test case.
-    </p>
-  </>
+  </div>
 ))
